305b5d9596b7519edc1057ea23ba6dd0ff3c0b9f
Mostafa Youns <mostafayouns27@gmail.com>
2025-10-14 01:38:41 +0300

    create registerPage structure and style

--- frontend/src/app/components/register-page/register-page.ts
+++ frontend/src/app/components/register-page/register-page.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './register-page.html',
-  styleUrl: './register-page.css'
+  styleUrl: './register-page.css',
 })
-export class RegisterPage {
-
-}
+export class RegisterPage {}
