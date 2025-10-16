401ad9ea42b95bfbb71d64ba0d0fbb744181dbb8
Mostafa Youns <mostafayouns27@gmail.com>
2025-10-16 17:50:42 +0300

    change styling

--- frontend/src/app/components/register-page/register-page.ts
+++ frontend/src/app/components/register-page/register-page.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './register-page.html',
   styleUrl: './register-page.css',
 })
-export class RegisterPage {}
+export class RegisterPage {
+  type = 'password';
+  myLogForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/),
+    ]),
+  });
+  get emailValid() {
+    return this.myLogForm.controls.email;
+  }
+  get passwordValid() {
+    return this.myLogForm.controls.password;
+  }
+  showHidePassword() {
+    if (this.type == 'password') this.type = 'text';
+    else this.type = 'password';
+  }
+}
